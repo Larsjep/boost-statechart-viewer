a5f539c1e4e4704e17cb58231076261627332439
Petr Silhavik <silhavik.p@gmail.com>
2013-04-05 10:41:18 +0200

    Now all file types are supported.

--- src/boost-statechart-viewer.sh
+++ src/boost-statechart-viewer.sh
@@ -3,7 +3,7 @@ set -e
 # while [ $1 ]
 
 for src in "$@"; do
-    s=${src%.cpp}
+    s=${src%.*}
     clang++ -Xclang -load -Xclang @libdir@/visualizer.so -Xclang -plugin -Xclang visualize-statechart -c $src
     dot -Tps $s.dot > $s.eps
     epstopdf $s.eps > $s.pdf
