809d7b081e8009b43cb29b09be7b04e4d0c539bb
Petr Silhavik <silhavik.p@gmail.com>
2013-05-16 09:18:46 +0200

    Update script for using boost statechart viewer
    Now it is possible to use more input files and treat them as one statechart. The first parameter must be -link

--- src/boost-statechart-viewer.sh
+++ src/boost-statechart-viewer.sh
@@ -2,6 +2,24 @@
 set -e
 # while [ $1 ]
 
+if [ "$1" == "-link" ]
+then
+    s=/tmp/statechart
+    echo > $s.cpp
+    reldir=`dirname $2`
+    cd $reldir
+    directory=`pwd`
+
+    for src in "${@:2}"; do
+	echo "#include \""$src"\"" >> $s.cpp
+    done
+    clang++ -Xclang -load -Xclang /home/petr/projects/boost-statechart-viewer/src/visualizer.so -Xclang -plugin -Xclang visualize-statechart -c $s.cpp
+    dot -Tps $s.dot > $s.eps
+    epstopdf $s.eps > $s.pdf
+    rm $s.dot $s.eps $s.cpp
+    mv $s.pdf $directory
+else
+
 for src in "$@"; do
     s=${src%.*}
     clang++ -Xclang -load -Xclang @libdir@/visualizer.so -Xclang -plugin -Xclang visualize-statechart -c $src
@@ -9,3 +27,5 @@ for src in "$@"; do
     epstopdf $s.eps > $s.pdf
     rm $s.dot $s.eps
 done
+
+fi
